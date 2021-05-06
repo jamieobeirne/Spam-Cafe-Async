36c816d49a4c901262014849d4a719af3c91a70b
Jamie OBeirne <jamieobeirne123@gmail.com>
2021-05-07 00:08:36 +0200

    6 may from PEC2

---
+++ src/index.js
@@ -0,0 +1,38 @@
+
+/*James O’Beirne
+M4.257 - Herramientas HTML y CSS aula 1
+PEC1. Desarrollo de una web
+16 de Abril de 2021*/
+
+/*https://github.com/fabricjs/fabric.js*/
+
+
+//import { fabric } from "fabric";
+
+const fabric = require("fabric").fabric;
+
+window.onload = function (event) {
+    var canvas = new fabric.Canvas('c');
+    var imgElement = document.getElementById('my-image');
+    var imgInstance = new fabric.Image(imgElement, {
+        left: 60,
+        top: 60,
+        angle: 30
+    });
+    canvas.add(imgInstance);
+}
+
+
+/*hamburger menu toggle on/off*/
+let hamburger_button = document.getElementById("toggle");
+let menu = document.querySelector(".small_sreen_header_navigation");
+let close_hamburger_button = document.getElementById("close_menu");
+menu.style.display = "none";
+
+hamburger_button.addEventListener("click", function display() {
+    menu.style.display = "block";
+});
+
+close_hamburger_button.addEventListener("click", function closeMenu() {
+    menu.style.display = "none";
+});
