7ef13b39ea7d4f6c95d90254897c30326e586379
Jamie OBeirne <jamieobeirne123@gmail.com>
2021-06-11 19:59:49 +0200

    final push before entrega

--- dist/style.e308ff8e.js
+++ dist/style.e308ff8e.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./images/spam_breakfast.jpeg":[["spam_breakfast.150e4540.jpeg","images/spam_breakfast.jpeg"],"images/spam_breakfast.jpeg"],"./images/spam_breakfast_reduced.jpeg":[["spam_breakfast_reduced.ef58d613.jpeg","images/spam_breakfast_reduced.jpeg"],"images/spam_breakfast_reduced.jpeg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./images/spam_breakfast.webp":[["spam_breakfast.f4fe218b.webp","images/spam_breakfast.webp"],"images/spam_breakfast.webp"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58193" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53188" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
